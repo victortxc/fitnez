55018e58398f4f6f9f29ef66f22577ff4ace3f87
Victor Medeiros <victormedeiros.pj@gmail.com>
2023-04-11 22:55:25 -0300

    add react routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {Login} from './screens/Login';
+import {NotFound} from './screens/NotFound';
+import {Home} from './screens/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path='home' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/screens/Login.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Login() {
+    return <h1>Tela de login</h1>
+}
