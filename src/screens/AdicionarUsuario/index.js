6880e5b56329946fd39fd9e8b97ac3819ea3fda3
viniciusgbsantos <vinicius.gabriel001@outlook.com>
2023-04-17 17:50:56 -0300

    Update

--- src/screens/AdicionarUsuario/index.js
+++ src/screens/AdicionarUsuario/index.js
@@ -1,140 +1,112 @@
 
 
 import React from "react";
-import { Header } from "../../components/Header";
-import { Input } from "../../components/Input";
-import { Formik, Form } from 'formik';
-import { getCep } from "../../actions/cep.js"
+import { useFormik } from 'formik';
 
 
 export function AdicionarUsuario() {
-  function onSubmit(values, actions) {
-    console.log('SUBMIT', values);
-  }
 
-  function preencherCep() {
-    const cep = this.cep.replace(/\D/g, "");
-    if (cep.length === 8) {
-      getCep(cep).then(response => {
-        this.rua = response.data.logradouro;
-        this.bairro = response.data.bairro;
-        this.estado = response.data.uf;
-        this.cidade = response.data.localidade;
-      });
-    }
 
-  return (
-      <>
-      <Header />
-      <Formik
-        onSubmit={() => {
-              preencherCep();
-            }}
-        validateOnMount
-        initialValues={{
-          nome: '',
-          idade: '', 
-          cep: '',
-          logradouro: '',
-          numero: '',
-          complemento: '',
-          bairro: '',
-          cidade: '',
-          formacao: '', 
-          experiencia: '', 
-          hobbies: ''
-        }}
-        >
-        {({
-              values,
-              handleChange,
-              handleBlur,
-              handleSubmit
-            }) => (
-            <Form onSubmit={{onSubmit}}>
-                <Input
-                  label="Nome:"
-                  type="text"
-                  name="nome"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.nome}
-                />
-                <Input
-                  label="Idade:"
-                  type="text"
-                  name="idade"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.idade}
-                />
-                <Input
-                  label="CEP:"
-                  type="text"
-                  name="cep"
-                  onChange={handleChange}
-                  value={values.cep}
-                  onBlur={handleBlur}
-                />
-                <button type="submit" onClick={preencherCep}>Buscar</button>
-                <Input
-                  label="Bairro:"
-                  type="text"
-                  name="bairro"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.bairro}
-                />
-                <Input
-                  label="Logradouro: "
-                  type="text"
-                  name="logradouro"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.logradouro}
-                />
-                <Input
-                  label="Nº"
-                  type="text"
-                  name="numero"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.numero}
-                />
-                <Input
-                  label="Cidade:"
-                  type="text"
-                  name="cidade"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.cidade}
-                />
-                <Input
-                  label="Formações:"
-                  type="text"
-                  name="formacao"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.formacao}
-                />
-                <Input
-                  label="Experiências:"
-                  type="text"
-                  name="experiencia"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.experiencia}
-                />
-                <Input
-                  label="Hobbies:"
-                  type="text"
-                  name="hobbies"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.hobbies}
-                />
-            <button type="submit">Enviar</button>
-          </Form>
-          )}
-          </Formik>
-    </>)}}
+  const SignupForm = () => {
+    const formik = useFormik({
+      initialValues: {
+        nome: '',
+        idade: '',
+        cep: '',
+        Logradouro: '',
+        numero: '',
+        cidade: '',
+        formacoes: '',
+        experiencias: '',
+        hobbies: '',
+      },
+      onSubmit: values => {
+        console.log(JSON.stringify(values, null, 2));
+      },
+    });
+    return (
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="nome">Nome: </label>
+        <input
+          id="nome"
+          name="nome"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.nome}
+        />
+        <label htmlFor="idade">Idade: </label>
+        <input
+          id="idade"
+          name="idade"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.idade}
+        />
+        <label htmlFor="cep">CEP</label>
+        <input
+          id="cep"
+          name="cep"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.cep}
+        />
+        <button type="submit">Buscar CEP</button>
+        <label htmlFor="logradouro">Logradouro</label>
+        <input
+          id="logradouro"
+          name="logradouro"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.logradouro}
+        />
+        <label htmlFor="bairro">Bairro</label>
+        <input
+          id="bairro"
+          name="bairro"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.bairro}
+        />
+        <label htmlFor="numero">Numero</label>
+        <input
+          id="numero"
+          name="numero"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.numero}
+        />
+        <label htmlFor="cidade">Cidade</label>
+        <input
+          id="cidade"
+          name="cidade"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.cidade}
+        />
+        <label htmlFor="formacoes">Formações</label>
+        <input
+          id="formacoes"
+          name="formacoes"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.formacoes}
+        />
+        <label htmlFor="experiencias">Experiências</label>
+        <input
+          id="experiencias"
+          name="experiencias"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.experiencias}
+        />
+        <label htmlFor="hobbies">Hobbies</label>
+        <input
+          id="hobbies"
+          name="hobbies"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.hobbies}
+        />
+      </form>
+    );
+  };  }
